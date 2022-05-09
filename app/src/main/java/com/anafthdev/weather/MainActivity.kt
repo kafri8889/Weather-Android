066979c081330cabbc86d1c3e525a6b6e73a8507
kafri8889 <kafri8889@gmail.com>
2022-05-09 17:31:21 +0700

    refactor: add permission request

--- app/src/main/java/com/anafthdev/weather/MainActivity.kt
+++ app/src/main/java/com/anafthdev/weather/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.anafthdev.weather
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.anafthdev.weather.databinding.ActivityMainBinding
+import com.anafthdev.weather.foundation.extension.toast
+import com.mapbox.mapboxsdk.Mapbox
 import dagger.hilt.android.AndroidEntryPoint
 import timber.log.Timber
 
@@ -10,6 +16,18 @@ import timber.log.Timber
 class MainActivity : AppCompatActivity() {
 	
 	private lateinit var binding: ActivityMainBinding
+	
+	private val resultLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+		if (!permissions.all { it.value }) {
+			getString(R.string.you_must_allow_location_access).toast(this)
+			finishAffinity()
+		}
+	}
+	
+	private val permissions = arrayOf(
+		Manifest.permission.ACCESS_FINE_LOCATION,
+		Manifest.permission.ACCESS_COARSE_LOCATION,
+	)
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -24,5 +42,11 @@ class MainActivity : AppCompatActivity() {
 		}
 		
 		setSupportActionBar(binding.toolbarMainActivity.toolbar)
+		
+		if (!permissions.all { ActivityCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED }) {
+			resultLauncher.launch(permissions)
+		}
+		
+		Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
 	}
 }
